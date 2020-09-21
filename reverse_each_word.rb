2c2f3cba44189a55e4a5253a6e52696756da5f67
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-21 19:48:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 sentence1 = ["Hello", "there,", "and", "how", "are", "you?"] 
 
-def reverse_each_word(sentence1)
+def reverse_each_word(word)
   sentence1.each do |word|
     puts word.reverse
   end
