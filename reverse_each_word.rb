7e2f132253c7d46b17a02912adb7805998de32a1
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-21 20:11:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,7 @@
 sentence1 = "Hello there, and how are you?"
 
-def reverse_each_word(sentence1)
-  sentence_split=
-  .each do |word|
+def reverse_each_word(sentence)
+  sentence.each do |word|
     word.reverse
   end
 end
